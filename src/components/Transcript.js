20db94719db42ee6b1d6923af0db8182617c235c
ashleykolodziej <kolodz10@bu.edu>
2021-06-05 14:29:04 -0400

    Maybe fix build error

--- src/components/Transcript.js
+++ src/components/Transcript.js
@@ -1,31 +1,44 @@
-import React from 'react'
+import React, { useRef, useEffect } from 'react'
 import PropTypes from 'prop-types'
 import { HTMLContent } from '../components/Content'
 
 const Transcript = ({ content, onSeekTo }) => {
-  const contentNode = document.createRange().createContextualFragment(content);
-  const paragraphs = contentNode.querySelectorAll('p');
+  const ref = useRef();
+  console.log(ref);
 
-  for (const paragraph of paragraphs) {
-      const paragraphText = paragraph.textContent,
-            timeStamp = paragraphText.substring(0, 10),
-            cleanedText = paragraphText.substring(10);
+  useEffect(() => {
+    const contentNode = document.createRange().createContextualFragment(content);
+    const paragraphs = contentNode.querySelectorAll('p');
 
-      const timeInSeconds = ( timeStamp ) => {
-        const splitTime = timeStamp.slice(1,-1).split(':');
-        return (splitTime[0]*60*60 + splitTime[1]*60 + parseInt(splitTime[2]));
-      };
+    for (const paragraph of paragraphs) {
+        const paragraphText = paragraph.textContent,
+              timeStamp = paragraphText.substring(0, 10),
+              cleanedText = paragraphText.substring(10);
 
-      paragraph.innerHTML = `<button type="button" data-time=${timeInSeconds(timeStamp)}>${timeStamp}</button>${cleanedText}</p>`;
+        const timeInSeconds = ( timeStamp ) => {
+          const splitTime = timeStamp.slice(1,-1).split(':');
+          return (splitTime[0]*60*60 + splitTime[1]*60 + parseInt(splitTime[2]));
+        };
+
+        paragraph.innerHTML = `<button type="button" data-time=${timeInSeconds(timeStamp)}>${timeStamp}</button>${cleanedText}</p>`;
+    }
+
+    let uselessDiv = document.createElement('div');
+        uselessDiv.appendChild( contentNode.cloneNode(true) );
+
+    ref.current = uselessDiv.innerHTML;
+
+    return;
+  }, [content]);
+
+  if (undefined === ref.current) {
+    return null;
   }
-
-  let uselessDiv = document.createElement('div');
-      uselessDiv.appendChild( contentNode.cloneNode(true) );
 
   return (
     <aside className="chapters" onClick={onSeekTo}>
       <h2>Transcript</h2>
-      <HTMLContent content={uselessDiv.innerHTML} />
+      <HTMLContent content={ref.current} />
     </aside>
   )
 }
